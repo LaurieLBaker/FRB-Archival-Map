412f6d78f1c5bd84eefeb287379305e723172f49
M Ilham AR Santoso <156228917+isantoso24@users.noreply.github.com>
2024-04-16 15:46:29 +0000

    finish animation

--- JS/animate-route-test.js
+++ JS/animate-route-test.js
@@ -8,29 +8,6 @@ const map = new mapboxgl.Map({
     pitch: 40
 });
 
-// San Francisco
-const origin = [-122.414, 37.776];
-
-// Washington DC
-const destination = [-77.032, 38.913];
-
-//Toronto
-const layover = [-79.421518,43.653241]
-
-// A simple line from origin to destination.
-const route = {
-    'type': 'FeatureCollection',
-    'features': [
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'LineString',
-                'coordinates': [origin, layover, destination]
-            }
-        }
-    ]
-};
-
 // A single point that animates along the route.
 // Coordinates are initially set to origin.
 const point = {
@@ -41,133 +18,166 @@ const point = {
             'properties': {},
             'geometry': {
                 'type': 'Point',
-                'coordinates': origin //why its only one point
+                'coordinates': [ -70.47166478, 43.11916619] //why its only one point
             }
         }
     ]
 };
 
-// Calculate the distance in kilometers between route start/end point.
-const lineDistance = turf.length(route.features[0]);
+// A simple line from origin to destination.
+// Read the GeoJSON data and add line layer
+fetch('Geojson-data/journals_test.geojson')
+    .then(response => response.json())
+    .then(data => {
+        // Extract coordinates from the point features
+        const coordinates = data.features.map(feature => feature.geometry.coordinates);
 
-const arc = [];
+        // Create a LineString geometry using the extracted coordinates
+        const lineString = {
+            type: 'LineString',
+            coordinates: coordinates
+        };
 
-// Number of steps to use in the arc and animation, more steps means
-// a smoother arc and animation, but too many steps will result in a
-// low frame rate
-const steps = 500;
+        // Create a new GeoJSON object containing the LineString feature
+        const route = {
+            type: 'FeatureCollection',
+            features: [{
+                type: 'Feature',
+                geometry: lineString,
+                properties: {} // You can add properties if needed
+            }]
+        };
 
-// Draw an arc between the `origin` & `destination` of the two points
-for (let i = 0; i < lineDistance; i += lineDistance / steps) {
-    const segment = turf.along(route.features[0], i);
-    arc.push(segment.geometry.coordinates);
-}
+        // Use or save the new GeoJSON object as needed
+        console.log(route); // Output the new GeoJSON object
 
-// Update the route with calculated arc coordinates
-route.features[0].geometry.coordinates = arc;
+        // Calculate the distance in kilometers between route start/end point.
+        const lineDistance = turf.length(route.features[0]);
 
-// Used to increment the value of the point measurement against the route.
-let counter = 0;
+        const arc = [];
 
-map.on('load', () => {
-    // Add a source and layer displaying a point which will be animated in a circle.
-    map.addSource('route', {
-        'type': 'geojson',
-        'data': route
-    });
+        // Number of steps to use in the arc and animation, more steps means
+        // a smoother arc and animation, but too many steps will result in a
+        // low frame rate
+        const steps = 500;
 
-    map.addSource('point', {
-        'type': 'geojson',
-        'data': point
-    });
-
-    map.addLayer({
-        'id': 'route',
-        'source': 'route',
-        'type': 'line',
-        'paint': {
-            'line-width': 2,
-            'line-color': '#007cbf'
+        // Draw an arc between the `origin` & `destination` of the two points
+        for (let i = 0; i < lineDistance; i += lineDistance / steps) {
+            const segment = turf.along(route.features[0], i);
+            arc.push(segment.geometry.coordinates);
         }
-    });
 
-    map.addLayer({
-        'id': 'point',
-        'source': 'point',
-        'type': 'symbol',
-        'layout': {
-            // This icon is a part of the Mapbox Streets style.
-            // To view all images available in a Mapbox style, open
-            // the style in Mapbox Studio and click the "Images" tab.
-            // To add a new image to the style at runtime see
-            // https://docs.mapbox.com/mapbox-gl-js/example/add-image/
-            'icon-image': 'airport',
-            'icon-size': 1.5,
-            'icon-rotate': ['get', 'bearing'],
-            'icon-rotation-alignment': 'map',
-            'icon-allow-overlap': true,
-            'icon-ignore-placement': true
-        }
-    });
-    let running = false;
-    function animate() {
-        running = true;
-        document.getElementById('replay').disabled = true;
-        const start =
-            route.features[0].geometry.coordinates[
+        // Update the route with calculated arc coordinates
+        route.features[0].geometry.coordinates = arc;
+
+        // Used to increment the value of the point measurement against the route.
+        let counter = 0;
+
+        // Add source for the line layer
+        map.addSource('route', {
+            type: 'geojson',
+            data: route // Pass the GeoJSON object
+        });
+
+        map.addSource('point', {
+            'type': 'geojson',
+            'data': point
+        });
+    
+        // Add line layer to show up
+        map.addLayer({
+            "id": "route",
+            "minzoom": 0,
+            "maxzoom": 22,
+            "type": "line",
+            "paint": {
+                'line-color': 'red',
+                'line-width': 2
+            },
+            "source": "route",
+        });
+
+        map.addLayer({
+            'id': 'point',
+            'source': 'point',
+            'type': 'symbol',
+            'layout': {
+                // This icon is a part of the Mapbox Streets style.
+                // To view all images available in a Mapbox style, open
+                // the style in Mapbox Studio and click the "Images" tab.
+                // To add a new image to the style at runtime see
+                // https://docs.mapbox.com/mapbox-gl-js/example/add-image/
+                'icon-image': 'ferry',
+                'icon-size': 1.5,
+                'icon-rotate': ['get', 'bearing'],
+                'icon-rotation-alignment': 'map',
+                'icon-allow-overlap': true,
+                'icon-ignore-placement': true
+            }
+        });
+        let running = false;
+        function animate() {
+            running = true;
+            document.getElementById('replay').disabled = true;
+            const start =
+                route.features[0].geometry.coordinates[
                 counter >= steps ? counter - 1 : counter
-            ];
-        const end =
-            route.features[0].geometry.coordinates[
+                ];
+            const end =
+                route.features[0].geometry.coordinates[
                 counter >= steps ? counter : counter + 1
-            ];
-        if (!start || !end) {
-            running = false;
-            document.getElementById('replay').disabled = false;
-            return;
-        }
-        // Update point geometry to a new position based on counter denoting
-        // the index to access the arc
-        point.features[0].geometry.coordinates =
-            route.features[0].geometry.coordinates[counter];
+                ];
+            if (!start || !end) {
+                running = false;
+                document.getElementById('replay').disabled = false;
+                return;
+            }
+            // Update point geometry to a new position based on counter denoting
+            // the index to access the arc
+            point.features[0].geometry.coordinates =
+                route.features[0].geometry.coordinates[counter];
 
-        // Calculate the bearing to ensure the icon is rotated to match the route arc
-        // The bearing is calculated between the current point and the next point, except
-        // at the end of the arc, which uses the previous point and the current point
-        point.features[0].properties.bearing = turf.bearing(
-            turf.point(start),
-            turf.point(end)
-        );
+            // Calculate the bearing to ensure the icon is rotated to match the route arc
+            // The bearing is calculated between the current point and the next point, except
+            // at the end of the arc, which uses the previous point and the current point
+            point.features[0].properties.bearing = turf.bearing(
+                turf.point(start),
+                turf.point(end)
+            );
 
-        // Update the source with this new data
-        map.getSource('point').setData(point);
-
-        // Request the next frame of animation as long as the end has not been reached
-        if (counter < steps) {
-            requestAnimationFrame(animate);
-        }
-
-        counter = counter + 1;
-    }
-
-    document.getElementById('replay').addEventListener('click', () => {
-        if (running) {
-            void 0;
-        } else {
-            // Set the coordinates of the original point back to origin
-            point.features[0].geometry.coordinates = origin;
-
-            // Update the source layer
+            // Update the source with this new data
             map.getSource('point').setData(point);
 
-            // Reset the counter
-            counter = 0;
+            // Request the next frame of animation as long as the end has not been reached
+            if (counter < steps) {
+                requestAnimationFrame(animate);
+            }
 
-            // Restart the animation
-            animate(counter);
+            counter = counter + 1;
         }
-    });
 
-    // Start the animation
-    animate(counter);
-});
+        document.getElementById('replay').addEventListener('click', () => {
+            if (running) {
+                void 0;
+            } else {
+                // Set the coordinates of the original point back to origin
+                point.features[0].geometry.coordinates = origin;
+    
+                // Update the source layer
+                map.getSource('point').setData(point);
+    
+                // Reset the counter
+                counter = 0;
+    
+                // Restart the animation
+                animate(counter);
+            }
+        });
+    
+        // Start the animation
+        animate(counter);
+
+    })
+    .catch(error => {
+        console.error('Error loading GeoJSON:', error);
+    });
