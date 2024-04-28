405c8ab0ee9532493bb537dd5e7d4ee3d28a2224
M Ilham AR Santoso <156228917+isantoso24@users.noreply.github.com>
2024-04-28 20:54:42 +0000

    add test.js

--- test.js
+++ test.js
@@ -1,34 +1,35 @@
 const searoute = require('searoute-js');
- 
+
 // Define origin and destination GeoJSON points:
 var origin = {
-  "type": "Feature",
-  "properties": {},
-  "geometry": {
-    "type": "Point",
-    "coordinates": [
-      132.5390625,
-      21.616579336740603
-    ]
-  }
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            132.5390625,
+            21.616579336740603
+        ]
+    }
 }
- 
+
 var destination = {
     "type": "Feature",
     "properties": {},
     "geometry": {
-      "type": "Point",
-      "coordinates": [
-        -71.3671875,
-        75.05035357407698
-      ]
+        "type": "Point",
+        "coordinates": [
+            -71.3671875,
+            75.05035357407698
+        ]
     }
-  }
- 
- 
+}
+
 var route = searoute(origin, destination);
 // > Returns a GeoJSON LineString Feature
- 
-// Optionally, define the units for the length calculation included in the properties object.
-// Defaults to nautical miles, can be degrees, radians, miles, or kilometers.
-var routeMiles = searoute(origin, destination, "miles");
+
+// Convert route object to a JSON string
+var routeJSON = JSON.stringify(route);
+
+// Log the GeoJSON data
+console.log(routeJSON);
